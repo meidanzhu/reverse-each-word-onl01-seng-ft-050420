cfd3acbd6a278420c386307e586b725783d98584
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-06 22:03:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,14 +4,15 @@ def reverse_each_word(sentence)
   string.each do |sentence|
   reverse_sentence << sentence.reverse
   end
-  return reversed_sentence.join(" ")
+  reversed_sentence.join(" ")
 end
 
 def reverse_each_word(sentence)
   string = sentence.split(" ")
-  test_string = []
+  reversed_sentence = []
   string.collect do|sentence|
-    test_string << sentence.reverse
+    reversed_sentence << sentence.reversed
   end
-  test_string.join(" ")
+  reversed_sentence.join(" ")
 end
+
